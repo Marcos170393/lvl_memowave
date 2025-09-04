005d2211451117a12d6a0c86ae721724e9e37b1a
Marcos170393 <larrosamarcos@gmail.com>
2025-09-03 23:54:21 -0300

    feat: ResponseHelper

---
+++ app/Helpers/ResponseHelper.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Helpers;
+
+class ResponseHelper {
+    public static function success($data = [], $status = 200){
+        return response()->json($data, $status);
+    }
+    public static function error($message = 'An error ocurred', $status = 500){
+        return response()->json(['message' => $message], $status);
+    }
+}
