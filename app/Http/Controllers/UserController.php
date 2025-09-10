cb5cc0c7a2a7cbd961bfe0fd312050393caf1f5a
Marcos170393 <larrosamarcos@gmail.com>
2025-09-10 19:37:33 -0300

    userservice: working

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,13 +8,14 @@ use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Exception;
 use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use stdClass;
 
 class UserController extends Controller
 {
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
         $data = (object) $request->validated();
         try {
@@ -35,11 +36,11 @@ class UserController extends Controller
         }
     }
 
-    public function store(StoreUserRequest $request)
+    public function store(StoreUserRequest $request): JsonResponse
     {
         $data = (object) $request->validated();
         try {
-            $user = new User;
+            $user = new User();
             $user->username = $data->username;
             $user->password = Hash::make($data->password);
             $result = User::insertGetId($user->toArray());
